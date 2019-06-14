752decaaf5231fcb95e534f99679eeb998f7f09c
seelow <seelow@qq.com>
2019-06-14 20:28:12 +0800

    first error: forget commit local repository

--- first.py
+++ first.py
@@ -1,1 +1,13 @@
-print('hello world!')
+import threading
+import time
+
+
+def test():
+    while True:
+        print('haha')
+        time.sleep(1)
+
+t = threading.Thread(target=test)
+t.start()
+print('marsk threading end')
+
